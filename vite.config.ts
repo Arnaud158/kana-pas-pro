133cc32a9dd0a275f0fac3ece6234ce781e2ff8e
Arnaud FIEVET <contact@arnaud-fievet.fr>
2026-04-26 11:35:04 +0200

    :bug: fix pwa url

--- vite.config.ts
+++ vite.config.ts
@@ -6,41 +6,46 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Kana Pas Pro',
-        short_name: 'Kana Pas Pro',
-        description: 'Learn hiragana & katakana fast and easy',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+export default defineConfig(() => {
+  const basePath = process.env.BASE_URL || '/'
+  return {
+    base: basePath,
+    plugins: [
+      vue(),
+      vueDevTools(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'Kana Pas Pro',
+          short_name: 'Kana Pas Pro',
+          description: 'Learn hiragana & katakana fast and easy',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          display: 'standalone',
+          start_url: basePath,
+          icons: [
+            {
+              src: `${basePath}android-chrome-192x192.png`,
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: `${basePath}android-chrome-512x512.png`,
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,wasm,css,html,ico,png,svg,woff,woff2,ttf,eot}'],
+        },
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
-      workbox: {
-        globPatterns: ['**/*.{js,wasm,css,html,ico,png,svg,woff,woff2,ttf,eot}'],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  },
+  }
 })
